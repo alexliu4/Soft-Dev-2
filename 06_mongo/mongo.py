f28de8ef7b7894196651dadb2b9e616cf36aebb5
alexliu4 <aliu9@stuy.edu>
2019-03-01 01:19:45 -0500

    working on assignment 06

---
+++ 06_mongo/mongo.py
@@ -0,0 +1,52 @@
+# Alex Liu, Josh Weiner
+# SoftDev2 pd6
+# K06 -- Yummy Mango Py
+# 2019-02-28
+
+import pymongo
+
+SERVER_ADDR = "167.99.158.108"
+connection = pymongo.MongoClient(SERVER_ADDR)
+db = connection.test
+collection = db.restaurants
+
+# All restaurants in a specified borough.
+def get_borough(borough):
+    return collection.find( {'borough': borough} )
+
+# All restaurants in a specified zip code.
+def get_zipcode(zipcode):
+    return collection.find( {'address.zipcode': zipcode} )
+
+# All restaurants in a specified zip code and with a specified grade.
+def get_zip_grade(zipcode, grade):
+    return collection.find( {'address.zipcode': zipcode, 'grades.grade': grade} )
+
+# All restaurants in a specified zip code with a score below a specified threshold.
+def get_zip_score(zipcode, score):
+    return collection.find( {'address.zipcode': zipcode, 'grades.score': {'$lt': score} } )
+
+# Something more clever
+def get_zip_grade_score(zipcode, grade, score):
+    return collection.find( {'address.zipcode': zipcode, 'grades.grade': grade, 'grades.score': {'$gt': score} } )
+
+def display_restaurants(collection):
+    for i in collection:
+        # print (i['name'])
+        print(i)
+
+
+print ("-----RESTAURANTS IN QUEENS-----\n")
+display_restaurants(get_borough("Queens"))
+
+print ("=====RESTAURANTS IN 10282=====\n")
+display_restaurants(get_zipcode("10282"))
+
+print ("=====RESTAURANTS IN 10282 WITH GRADE A=====\n")
+display_restaurants(get_zip_grade("10282", "A"))
+
+print ("=====RESTAURANTS IN 10282 WITH SCORE LOWER THAN 7=====\n")
+display_restaurants(get_zip_score("10282", 5))
+
+print ("=====RESTAURANTS IN 10282 WITH GRADE A AND SCORE GREATER THAN 20=====\n")
+display_restaurants(get_zip_grade_score("10282", "A", 20))
