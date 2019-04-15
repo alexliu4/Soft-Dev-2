7ffbe9a503dc08a7af141074de5c6ade55413231
alexliu4 <aliu9@stuy.edu>
2019-04-14 23:01:56 -0400

    finished number 7

--- 17/listcom.py
+++ 17/listcom.py
@@ -13,19 +13,11 @@ def loopyOne():
         var += 2
     return num
 
-
-print("1A: ")
-print(loopyOne())
-
 # 1b
 def listcompOne():
     nums = "02468"
     array = [x + x for x in nums]
     return array
-
-
-print("1B: ")
-print(listcompOne())
 
 # 2. [7, 17, 27, 37, 47]
 # 2a
@@ -37,19 +29,11 @@ def loopyTwo():
         var += 1
     return num
 
-
-print("2A: ")
-print(loopyTwo())
-
 # 2b
 def listcompTwo():
     nums = "01234"
     array = [ int(x + "7") for x in nums]
     return array
-
-
-print("2B: ")
-print(listcompTwo())
 
 # 3. [0, 0, 0, 0, 1, 2, 0, 2, 4]
 # 3a
@@ -67,10 +51,6 @@ def loopyThree():
     num.pop()
     return num
 
-
-print("3A: ")
-print(loopyThree())
-
 # 3b
 def listcompThree():
     nums = "012"
@@ -80,10 +60,6 @@ def listcompThree():
     array = array0 + array1 + array2
 
     return array
-
-
-print("3B: ")
-print(listcompThree())
 
 # helper function
 def primeFinder():
@@ -115,23 +91,9 @@ def loopyFour():
         num.remove(x)
     return num
 
-
-print("4A: ")
-print(loopyFour())
-
 # 4b
 def listcompFour():
-    nums = "012"
-    array0 = [ int(x) * 0 for x in nums]
-    array1 = [ int(x) * 1 for x in nums]
-    array2 = [ int(x) * 2 for x in nums]
-    array = array0 + array1 + array2
-
-    return array
-
-
-print("4B: ")
-print(listcompFour())
+    return [i for i in range(2, 101) if 1 in [1 if i % j == 0 else 0 for j in range(2, i)]]
 
 # 5. Primes on range [0,100], in ascending order.
 # 5a
@@ -140,27 +102,29 @@ def loopyFive():
     num.reverse()
     return num
 
-
-print("5A: ")
-print(loopyFive())
-
 # 5b
 def listcompFive():
-    nums = "012"
-    array0 = [ int(x) * 0 for x in nums]
-    array1 = [ int(x) * 1 for x in nums]
-    array2 = [ int(x) * 2 for x in nums]
-    array = array0 + array1 + array2
+    return [i for i in range(2, 101) if 1 not in [1 if i % j == 0 else 0 for j in range(2, i)]]
 
-    return array
+# 6a
+def loopySix(n):
+    i = 1
+    list = []
+    while (i < n):
+        if not n % i:
+            list.append(i)
+        i += 1
+    list.append(n)
+    return list
 
-
-print("5B: ")
-print(listcompFive())
+# 6b
+def listcompSix(n):
+    list = [i for i in range(n) if i > 0 and n % i == 0]
+    list.append(n)
+    return list
 
 
 # 7. transpose matrix
-print("7:: ")
 
 # i = inner list; j = outer list
 def transpose_matrix(matrix):
@@ -172,15 +136,38 @@ def transpose_matrix(matrix):
 
     return transpose
 
-
-array = [[0, 1], [2, 3], [4, 5]]
-mat = transpose_matrix(array)
-print(mat)
-
 def transpose_matrix_listcomp(matrix):
     transpose = [ [ matrix[j][i] for j in range(len(matrix)) ] for i in range(len(matrix[0])) ]
     return transpose
 
-
+print( " 1: ? -> ['00', '22', '44', '66', '88'] ")
+print(" 1A: " + str(loopyOne()))
+print(" 1B: " + str(listcompOne()))
+print ("\n==========================================\n")
+print( " 2: ? -> [7, 17, 27, 37, 47] ")
+print( " 2A: " + str(loopyTwo()))
+print( " 2B: " + str(listcompTwo()))
+print ("\n==========================================\n")
+print( " 3: ? -> [0, 0, 0, 0, 1, 2, 0, 2, 4] ")
+print( " 3A: " + str(loopyThree()))
+print( " 3B: " + str(listcompThree()))
+print ("\n==========================================\n")
+print( " 4: Composites on range [0,100], in ascending order. ")
+print( " 4A: " + str(loopyFour()))
+print( " 4B: " + str(listcompFour()))
+print ("\n==========================================\n")
+print( " 5: Primes on range [0,100], in ascending order. ")
+print( " 5A: " + str(loopyFive()))
+print( " 5B: " + str(listcompFive()))
+print ("\n==========================================\n")
+print( " 6: All divisors of a number, listed in ascending order. ")
+print( " 6A: " + str(loopySix(100)))
+print( " 6B: " + str(listcompSix(100)))
+print ("\n==========================================\n")
+print( " 7: Transpose a matrix (turn rows into columns and vice-versa...). ")
+array = [[0, 1], [2, 3], [4, 5]]
+mat = transpose_matrix(array)
 matLC = transpose_matrix_listcomp(array)
-print(matLC)
+print( " 7A: " + str(mat))
+print( " 7B: " + str(matLC))
+print ("\n==========================================\n")
