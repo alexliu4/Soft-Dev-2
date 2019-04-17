32982a0cfa0fff44a6a6cc2d7c02ca69b1c8e98a
alexliu4 <aliu9@stuy.edu>
2019-04-17 02:30:49 -0400

    working on assignments

--- 18_listcomp/listcomp.py
+++ 18_listcomp/listcomp.py
@@ -7,3 +7,8 @@ def triples(n):
     return [(x,y,z) for x in range(1,n) for y in range(1,n) for z in range(1,n) if x*x+y*y == z*z ]
 
 print triples(100)
+
+def quickSort(l):
+    return ((quickSort([y for y in l if y < l[0]]) + [l[0]] + quickSort([x for x in l if x > l[0]])) if len(l) > 2 else l)
+
+print(quickSort([9,42359,7,6,4895,4,4350834,1,12]))
