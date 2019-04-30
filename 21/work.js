01b5e5503e76f0063cd578fcdd32c333028ce395
alexliu4 <aliu9@stuy.edu>
2019-04-30 01:06:21 -0400

    working on 21

--- 21/work.js
+++ 21/work.js
@@ -1,21 +1,17 @@
-d3.csv("./NYC.csv").then(function(data){
-    data.forEach(d => {
-        d["grade1"] =+ d["grade1"]
-    })
-    var sumall= function(a,b) {
-        return a["total_enrollment"]+b["total_enrollment"]
-    };
-    var PS15=data.filter(function(n){
-        return (n.DBN=="01M188");
-    })
-    console.log(PS15)
-    var pop=PS15.map(function(n){
-        return (parseInt(n.total_enrollment))
-    }
-    )
-    console.log(pop)
-    var sumps=pop.reduce(function(a,b){
-        return a+b;
-    });
-    console.log(sumps)
-})
+d3.csv("https://raw.githubusercontent.com/alexliu4/Soft-Dev-2/master/21/2006_-_2012_School_Demographics_and_Accountability_Snapshot.csv").then(function(data){
+  console.log(data);
+
+// number of students in grade one
+  var grade1 =[];
+  for (var i = 0;i < data.length;i++){
+    grade1.push(data[i]["grade1"]);
+  }
+  console.log(grade1);
+
+  var grade1 = grade1.filter(function(n){return (Number.isInteger(parseInt(n)))});
+  var average1 = (grade1.reduce(function(a,b){return parseInt(a)+parseInt(b)}) / grade1.length);
+  console.log(average1)
+
+  var first = document.getElementById("avg");
+  first.innerHTML = average1;
+});
