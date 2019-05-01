8d08f159a54751dd76188c619f44b1ef60ab3abd
alexliu4 <aliu9@stuy.edu>
2019-05-01 02:17:27 -0400

    finished

--- 21_js-mfr/work.js
+++ 21_js-mfr/work.js
@@ -1,27 +1,38 @@
+
 d3.csv("https://raw.githubusercontent.com/alexliu4/Soft-Dev-2/master/21_js-mfr/2006_-_2012_School_Demographics_and_Accountability_Snapshot.csv").then(function(data){
-  console.log(data);
+  var PS034 = data.filter(function(n){
+	return (n.DBN == "01M034");
+    });
+    console.log(PS034);
 
-// number of students in grade one
-  var grade1 =[];
-  for (var i = 0;i < data.length;i++){
-    grade1.push(data[i]["grade1"]);
-  }
-  console.log(grade1);
+    //===================POPULATION==================
+    var population = PS034.map(function(n){
+        return (parseInt(n.total_enrollment))
+    });
+    console.log(population)
+    var total_pop = population.reduce(function(a,b){
+        return a + b;
+    });
+    console.log(total_pop);
+    document.getElementById("pop").innerHTML = total_pop;
 
-  var grade1 = grade1.filter(function(n){return (Number.isInteger(parseInt(n)))});
-  var average = (grade1.reduce(function(a,b){return parseInt(a)+parseInt(b)}) / grade1.length);
-  console.log(average)
+    //=====================AVG grade1======================
+    var grade1 = PS034.map(function(n){
+        return (parseInt(n.grade1))
+    });
+    console.log(grade1)
+    var total_grade1 = grade1.reduce(function(a,b){
+	return a + b;
+    });
+    console.log(total_grade1);
+    document.getElementById("grade1").innerHTML = "The average enrollment of pre-schoolers in PS034 over the course of 7 years is " + total_grade1/7;
 
-  var senior = data.filter(function(n) {
-    return n.grade12 >0})
-
-  var male = data.filter(function(n) {
-    return n.male_per > n.female_per})
-
-  var first = document.getElementById("avg");
-  first.innerHTML = average;
-  var first = document.getElementById("senior");
-  first.innerHTML = senior;
-  var first = document.getElementById("gender");
-  first.innerHTML = male;
-});
+    //==================BLACKS=====================
+    var year = PS034.filter(function(n){
+	return (n.schoolyear=="20092010");
+    });
+    console.log(year);
+    var black = year[0].black_num;
+    console.log(black);
+    document.getElementById("black").innerHTML = black;
+})
